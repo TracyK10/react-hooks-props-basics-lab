fcee3c40a4f40deccde99eba8cef1b309507dca6
Tracy Karanja <chirutracy@gmail.com>
2024-04-24 23:01:07 +0300

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function About() {
+function About({bio, links}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href={links.github}>{links.github}</a>
+      <a href={links.linkedin}>{links.linkedin}</a>
     </div>
   );
 }
